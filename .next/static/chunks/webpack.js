dfb6fcc6942bf170ecba9ebd7dd0d7f65af86d8e
yunsejin <100738553+yunsejin@users.noreply.github.com>
2024-04-21 19:32:15 +0900

    fix styles

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "2a1e98a46458297c"; }
+/******/ 		__webpack_require__.h = function() { return "6e2d14f80fdb76f8"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/pages/styles.ts
+++ src/pages/styles.ts
@@ -1,17 +1,51 @@
-// styles.ts
 import styled from 'styled-components';
 
 interface ButtonProps {
-  $left: number;
   color: string;
 }
 
+export const Title = styled.h1`
+  text-align: center;
+  font-size: 64px;
+  color: #000;
+  margin-top: 30px;
+  margin-bottom: 30px;
+
+  @media (max-width: 600px) {
+    font-size: 32px;
+  }
+`;
+
+export const ImageContainer = styled.div`
+  width: 100%;
+  max-width: 985px;
+  height: 453px;
+  margin: auto;
+  background-image: url('/image.png');
+  background-size: cover;
+  left: 50%;
+  transform: translateX(2%);
+
+  @media (max-width: 600px) {
+    height: 200px;
+  }
+`;
+
+export const ButtonContainer = styled.div`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  width: 100%;
+  max-width: 985px;
+  margin: auto;
+  margin-top: 30px;
+
+`;
+
 export const Button = styled.button<ButtonProps>`
-  position: absolute;
-  width: 153px;
+  flex-grow: 1;
+  margin: 10px;
   height: 83px;
-  left: ${props => props.$left}px;
-  top: 781px;
   background-color: ${props => props.color};
   font-family: 'JejuGothic', sans-serif;
   font-size: 48px;
@@ -21,47 +55,8 @@ export const Button = styled.button<ButtonProps>`
   cursor: pointer;
 
   @media (max-width: 600px) {
-    width: 76px;
-    height: 41px;
     font-size: 24px;
     line-height: 26px;
-  }
-`;
-
-export const Container = styled.div`
-  position: absolute;
-  width: 100%;
-  max-width: 985px;
-  height: 453px;
-  left: 50%;
-  transform: translateX(-50%);
-  top: 286px;
-  background-image: url('image.png');
-  background-size: cover;
-  background-position: center;
-
-  @media (max-width: 600px) {
-    height: 200px;
-  }
-`;
-
-export const Title = styled.h1`
-  position: absolute;
-  width: 100%;
-  max-width: 675px;
-  height: 89px;
-  left: 50%;
-  transform: translateX(-50%);
-  top: 129px;
-  font-family: 'JejuGothic', sans-serif;
-  font-size: 64px;
-  line-height: 69px;
-  color: #000;
-  text-align: center;
-
-  @media (max-width: 600px) {
-    font-size: 32px;
-    line-height: 36px;
-    height: 45px;
+    height: 41px;
   }
 `;
